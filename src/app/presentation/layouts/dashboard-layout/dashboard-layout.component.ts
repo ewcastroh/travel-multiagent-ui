26e15b86c0109d0ea330f3bdf879a2f79b9ff1d0
Eimer Castro <ewcastroh10@gmail.com>
2025-04-27 17:32:59 -0500

    Implementing dark mode toggle.

--- src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, OnInit, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { routes } from '../../../app.routes';
 import { SideBarMenuItemComponent } from '@components/index';
@@ -15,8 +15,28 @@ CommonModule,
   templateUrl: './dashboard-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardLayoutComponent {
+export class DashboardLayoutComponent implements OnInit {
   public routes = routes[0].children?.filter((route) => route.data);
 
   public darkMode = signal<boolean>(false);
+
+  constructor() {
+    effect(() => {
+      const body = document.body;
+      if (this.darkMode()) {
+        body.classList.add('dark-mode-body');
+        localStorage.setItem('darkMode', 'true');
+      } else {
+        body.classList.remove('dark-mode-body');
+        localStorage.setItem('darkMode', 'false');
+      }
+    });
+  }
+
+  public ngOnInit(): void {
+    const storedDarkMode = localStorage.getItem('darkMode');
+    if (storedDarkMode === 'true') {
+      this.darkMode.set(true);
+    }
+  }
 }
