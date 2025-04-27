581d2636ba507a73116527c83b5a40c0a22a1005
Eimer Castro <ewcastroh10@gmail.com>
2025-04-27 17:09:48 -0500

    Preparing style for dark mode.

--- src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { routes } from '../../../app.routes';
 import { SideBarMenuItemComponent } from '@components/index';
@@ -17,4 +17,6 @@ CommonModule,
 })
 export class DashboardLayoutComponent {
   public routes = routes[0].children?.filter((route) => route.data);
+
+  public darkMode = signal<boolean>(false);
 }
